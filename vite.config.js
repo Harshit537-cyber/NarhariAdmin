aab0cc3849362a91bc2bf865330309eeb1251e00
Ritik saini <saini.ritik@pearlorganisation.com>
2026-08-04 10:30:25 +0530

    Save local changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
+
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+
   plugins: [
+
     react(),
+
     tailwindcss(),
   ],
-   server: {
+   server:
+    {
     allowedHosts: [
+      
       'https://visionary-biscotti-cc607a.netlify.app/login',
     ],
   },
