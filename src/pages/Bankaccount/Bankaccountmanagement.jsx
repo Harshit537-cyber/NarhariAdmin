764c0cf284586382f5977f0e260ebf294bbd885d
Ritik saini <saini.ritik@pearlorganisation.com>
2026-09-22 10:27:54 +0530

    bank account done

--- src/pages/Bankaccount/Bankaccountmanagement.jsx
+++ src/pages/Bankaccount/Bankaccountmanagement.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react";
 import { getAllPartnerBankAccounts } from "../../api/Controller/bankaccount";
-import "./BankAccountmanagement.css";
+import "./Bankaccountmanagement.css";
 import { toast } from "react-toastify";
 import {
   FaCrown,
